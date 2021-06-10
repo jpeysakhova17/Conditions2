15a030589cc9e910b71ccb0fc373c7a7421827a4
Jenny <jenny@elis-MacBook-Air.local>
2021-06-09 21:47:24 -0400

    done

---
+++ script.js
@@ -0,0 +1,9 @@
+var name1 = "Jenny";
+var name2 = "Jennifer";
+var name3 = "Jen";
+
+if (name1.length > name2.length && name1.length > name3.length) {
+console.log ("Jenny has the longest name");
+} else if (name2.length > name1.length && name2.length > name3.length) {
+    console.log ("Jennifer has the longest name");
+} else ("Jen has the longest name");
