f29bd878a518c57341da21fbd3c7dcb8215caa88
Jenny <jenny@elis-MacBook-Air.local>
2021-06-09 22:13:33 -0400

    final

--- script.js
+++ script.js
@@ -6,4 +6,14 @@ if (name1.length > name2.length && name1.length > name3.length) {
 console.log ("Jenny has the longest name");
 } else if (name2.length > name1.length && name2.length > name3.length) {
     console.log ("Jennifer has the longest name");
-} else ("Jen has the longest name");
+} else if (name3.length > name1.length && name3.length > name2.length) {
+    console.log ("Jen has the longest name");
+} else if (name1.length === name2.length && name2.length === name3.length) {
+    console.log ("All three names are the same length");
+} else if (name1.length === name2.length && name1.length > name3.length) {
+    console.log("Jenny and Jennifer are tied for the longest name");
+} else if (name2.length === name3.length && name3.length > name1.length) {
+    console.log ("Jennifer and Jen are tied for the longest name");
+} else if (name1.length === name3.length && name1.length > name2.length) {
+    console.log ("Jenny and Jen are tied for the longest name");
+}
